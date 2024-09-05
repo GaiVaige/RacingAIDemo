16782f4697a2447367eecd02df83ab00302496b6
GaiVaige <thomas.n.c.evangelou@gmail.com>
2024-09-05 16:02:02 +1000

    InitPush

---
+++ RaylibStarterCPP/Flag.h
@@ -0,0 +1,184 @@
+#pragma once
+#include "raylib.h"
+#include "raymath.h"
+#include "vector"
+#include <iostream>
+class Racer {
+public:
+	Vector2 m_pos;
+	float m_progThroughTrack = 0;
+};
+
+struct Collider {
+	Collider(Vector2 max, Vector2 ori, float ang) : m_max(max), m_origin(ori), m_rot(ang) {
+		m_min = Vector2{ 0,0 };
+		m_w = m_max.x;
+		m_h = m_max.y;
+		float nMinX = INFINITY;
+		float nMinY = INFINITY;
+		float nMaxX = -INFINITY;
+		float nMaxY = -INFINITY;
+		m_verts[0] = m_min;
+		m_verts[1] = {m_max.x, m_min.y};
+		m_verts[2] = m_max;
+		m_verts[3] = Vector2{m_min.x, m_max.y};
+		GenerateMid();
+		for (int i = 0; i < 4; i++) {
+			m_verts[i] = Vector2Rotate(m_verts[i], m_rot);
+			m_verts[i] = Vector2Add(m_verts[i], m_origin);
+		}
+
+		GenerateNormals();
+	}
+	Vector2 m_min, m_max, m_origin;
+	Vector2 m_verts[4];
+	Vector2 m_norms[4];
+	Vector2 m_midPoint;
+	float m_w, m_h;
+	float m_rot;
+	bool m_overlappedLastFrame;
+	bool picked = false;
+
+	void Draw() {
+		DrawLine(m_verts[0].x, m_verts[0].y, m_verts[1].x, m_verts[1].y, RED);
+		DrawLine(m_verts[1].x, m_verts[1].y, m_verts[2].x, m_verts[2].y, RED);
+		DrawLine(m_verts[2].x, m_verts[2].y, m_verts[3].x, m_verts[3].y, RED);
+		DrawLine(m_verts[3].x, m_verts[3].y, m_verts[0].x, m_verts[0].y, RED);
+
+	}
+
+	void GenerateMid() {
+		float mx = m_max.x;
+		float my = m_max.y;
+		mx /= 2;
+		my /= 2;
+		m_midPoint = Vector2{ mx, my };
+		m_midPoint = Vector2Rotate(m_midPoint, m_rot);
+		m_midPoint = Vector2Add(m_origin, m_midPoint);
+	}
+
+	void DrawStart() {
+		Rectangle r{ m_origin.x, m_origin.y, 30, 75 };
+		DrawRectanglePro(r, Vector2{ 0,0 }, m_rot, GREEN);
+	}
+
+	void DrawHighlighted() {
+			Rectangle r{ m_origin.x, m_origin.y, 30, 75 };
+		if (!picked) {
+			DrawRectanglePro(r, Vector2{0,0}, m_rot, SKYBLUE);
+		}
+		else {
+			DrawRectanglePro(r, Vector2{ 0,0 }, m_rot, BLUE);
+
+		}
+	}
+
+	void UpdatePosition(Vector2 v, float damp = 1) {
+		if (damp == 0) return;
+		float mult = Vector2Length(v)/damp;
+		v = Vector2Normalize(v);
+		if (isnan(v.x) || isnan(v.y)) return;
+		v.x *= mult;
+		v.y *= mult;
+		for (int i = 0; i < 4; i++) {
+			m_verts[i] = Vector2Subtract(m_verts[i], m_origin);
+			m_verts[i] = Vector2Rotate(m_verts[i], -m_rot);
+				m_verts[i] = Vector2Rotate(m_verts[i], m_rot);
+				m_verts[i] = Vector2Add(m_verts[i], Vector2Add(m_origin, v));
+		}
+		m_origin = Vector2Add(m_origin, v);
+		GenerateMid();
+	}
+
+	void UpdateRotation(float nRot) {
+		if (isnan(nRot)) return;
+		for (int i = 0; i < 4; i++) {
+			m_verts[i] = Vector2Subtract(m_verts[i], m_origin);
+			m_verts[i] = Vector2Rotate(m_verts[i], -m_rot);
+			m_verts[i] = Vector2Rotate(m_verts[i], m_rot + nRot);
+			m_verts[i] = Vector2Add(m_verts[i], m_origin);
+		}
+		m_rot += nRot;
+		GenerateMid();
+	}
+
+	bool CheckForOverlap(Vector2 v) {
+		v = Vector2Subtract(v, m_origin);
+		v = Vector2Rotate(v, -m_rot);
+		bool inBoundX = (v.x > m_min.x && v.x < m_max.x);
+		bool inBoundY = (v.y > m_min.y && v.y < m_max.y);
+
+		return (inBoundX && inBoundY);
+	}
+
+	bool CheckForEntry(Vector2& v) {
+		bool isOverlapped = CheckForOverlap(v);
+		if (isOverlapped) {
+			if (!m_overlappedLastFrame) {
+			m_overlappedLastFrame = true;
+			return true;
+			}
+			else {
+				return false;
+			}
+		}
+		else {
+			m_overlappedLastFrame = false;
+			return false;
+		}
+	}
+
+private:
+	void GenerateNormals() {
+		for (int i = 0; i < 4; i++) {
+			int x = i;
+			if (x == 3) {
+				x = -1;
+			}
+			m_norms[i] = GenN(m_verts[i], m_verts[x + 1]);
+		}
+	}
+	Vector2 GenN(Vector2 a, Vector2 b) {
+		float dx, dy;
+		dx = b.x - a.x;
+		dy = b.y - a.y;
+		return Vector2Normalize(Vector2{ dy, -dx });
+	}
+
+
+};
+
+
+
+class Flag {
+public:
+	Flag(Vector2 ori, Vector2 cSize, Color col, float rot) : m_pos(ori), m_dCol(col) {
+		coll = new Collider(cSize, ori, rot);
+	}
+	~Flag() {};
+	Vector2 m_pos, m_dirToNext;
+	size_t flagIndex;
+	Color m_dCol;
+	Collider* coll;
+	Flag* m_nextFlag = nullptr;
+	Flag* m_previousFlag = nullptr;
+	bool m_hasPassed;
+
+};
+
+class SubFlag : public Flag {
+public:
+	SubFlag();
+};
+
+class MainFlag : public Flag {
+public:
+	MainFlag();
+
+	std::vector<SubFlag*> m_subFlags;
+	MainFlag* m_nextMainFlag;
+};
+
+class FinishFlag : public Flag {
+	std::vector<Flag*> flags;
+};
---
+++ RaylibStarterCPP/main.cpp
@@ -0,0 +1,292 @@
+/*******************************************************************************************
+*
+*   raylib [core] example - Basic window
+*
+*   Welcome to raylib!
+*
+*   To test examples, just press F6 and execute raylib_compile_execute script
+*   Note that compiled executable is placed in the same folder as .c file
+*
+*   You can find all basic examples on C:\raylib\raylib\examples folder or
+*   raylib official webpage: www.raylib.com
+*
+*   Enjoy using raylib. :)
+*
+*   This example has been created using raylib 1.0 (www.raylib.com)
+*   raylib is licensed under an unmodified zlib/libpng license (View raylib.h for details)
+*
+*   Copyright (c) 2014 Ramon Santamaria (@raysan5)
+*
+********************************************************************************************/
+
+#pragma warning( push , 0)
+#include <raylib.h>
+
+#define RAYGUI_IMPLEMENTATION
+#define RAYGUI_SUPPORT_ICONS
+#include <raygui.h>
+#include "Flag.h"
+#include <iostream>
+#pragma warning(pop)
+
+void ClearFlags(std::vector<Flag*>& ff) {
+    for (Flag* f : ff) {
+        delete f;
+    }
+    ff.clear();
+
+};
+
+int main(int argc, char* argv[])
+{
+    // Initialization
+    //--------------------------------------------------------------------------------------
+    int screenWidth = 1600;
+    int screenHeight = 900;
+
+    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+
+
+    std::vector<Flag*> flags;
+    float spawnRot = 180;
+    size_t lapCounter = 0;
+    SetTargetFPS(60);
+
+    bool picking = false;
+    bool drawingModeEnabled = true;
+    bool enableTrackSmoothing = false;
+    //--------------------------------------------------------------------------------------
+    Collider* hoveredCollider = nullptr;
+    int storedInd = 0;
+    Vector2 lastFrameMPose;
+
+    bool justSwapped = false;
+        system("CLS");
+            std::cout << "DRAWING MODE ENABLED" << '\n';
+    // Main game loop
+    while (!WindowShouldClose())    // Detect window close button or ESC key
+    {
+        // Update
+        //----------------------------------------------------------------------------------
+        // TODO: Update your variables here
+        //----------------------------------------------------------------------------------
+
+        // Draw
+        //----------------------------------------------------------------------------------
+        BeginDrawing();
+        DrawText("Swap between drawing and testing mode with V", 2, 0, 20, BLACK);
+        DrawText("Clear all flags with C", 2, 20, 20, BLACK);
+        DrawText("Enable track smoothing with S", 2, 40, 20, BLACK);
+        DrawText("Swap to picking mode with W", 2, 60, 20, BLACK);
+        ClearBackground(RAYWHITE);
+        Vector2 v = GetMousePosition();
+
+        if (!picking) {
+            spawnRot -= GetMouseWheelMove() * 5;
+        }
+        
+        
+        if (IsKeyPressed(KEY_V)) {
+            system("CLS");
+            std::cout << "DRAWING MODE ENABLED" << '\n';
+            drawingModeEnabled = !drawingModeEnabled;
+            justSwapped = !drawingModeEnabled;
+        }
+
+        if (drawingModeEnabled) {
+            if (IsKeyPressed(KEY_C)) {
+                ClearFlags(flags);
+            }
+
+            if (IsKeyPressed(KEY_S)) {
+                enableTrackSmoothing = !enableTrackSmoothing;
+            }
+
+            if (IsKeyPressed(KEY_W)) {
+                picking = !picking;
+            }
+
+        }
+
+
+        if (drawingModeEnabled) {
+            if (!picking) {
+                Rectangle r{ v.x, v.y, 30, 75};
+                DrawRectanglePro(r, Vector2{ 0,0 }, spawnRot, RED);
+            }
+            else {
+                for (Flag* f : flags) {
+                    if (f->coll->CheckForOverlap(v)) {
+                        hoveredCollider = f->coll;
+                        storedInd = f->flagIndex;
+                        if (IsMouseButtonDown(1)) {
+                            hoveredCollider->picked = true;
+
+                            Vector2 addVec = Vector2Subtract(lastFrameMPose, v);
+                            float getNRot = GetMouseWheelMove() * 5;
+                            hoveredCollider->UpdateRotation(-getNRot);
+                            if (!isnan(addVec.x) || !isnan(addVec.y)) {
+                                hoveredCollider->UpdatePosition(Vector2Negate(addVec));
+
+                                if (storedInd != 0) {
+                                    flags[storedInd - 1]->coll->UpdatePosition(Vector2Negate(addVec), 2);
+                                    flags[storedInd - 1]->coll->UpdateRotation(-getNRot * .2);
+
+                                }
+                                if (storedInd != flags.size() - 1) {
+                                    flags[storedInd + 1]->coll->UpdatePosition(Vector2Negate(addVec), 2);
+                                    flags[storedInd + 1]->coll->UpdateRotation(-getNRot * .2);
+                                }
+
+                            }
+
+
+                        }
+                        f->coll->DrawHighlighted();
+
+                    }
+                    else {
+                        if (hoveredCollider != nullptr) {
+                            hoveredCollider->picked = false;
+                            hoveredCollider = nullptr;
+                        }
+                    }
+                }
+            }
+
+            if (IsMouseButtonPressed(0) && !picking) {
+                int addedSegments = 1;
+                Flag* f = new Flag(v, Vector2{ 30, 75 }, BLUE, spawnRot);
+                std::cout << "FLAG CREATED AT LOCATION: " << v.x << " | " << v.y << '\n';
+                f->flagIndex = flags.size();
+
+
+                if (f->flagIndex != 0) {
+                    int amntToAdd;
+                    if (enableTrackSmoothing) {
+                         amntToAdd = (((int)f->coll->m_origin.x - flags[f->flagIndex - 1]->coll->m_origin.x) +
+                            ((int)f->coll->m_origin.y - flags[f->flagIndex - 1]->coll->m_origin.y) * 1.2)/50;
+                        amntToAdd = abs(amntToAdd);
+
+                        if (amntToAdd != 0) {
+                            Flag* checkF = flags[f->flagIndex - 1];
+                            Vector2 diffVec = Vector2Subtract(f->coll->m_origin, checkF->coll->m_origin);
+                            diffVec = Vector2Divide(diffVec, amntToAdd + 1);
+                            int rotDiff = checkF->coll->m_rot - f->coll->m_rot;
+                            float addedRot = 0;
+
+                            for (int i = 0; i < amntToAdd; i++) {
+                                addedRot += abs(rotDiff / (amntToAdd * 3));
+                                Flag* nf = new Flag(Vector2Add(checkF->coll->m_origin, Vector2MultiplyV(diffVec, Vector2{ (float)i + 1 , (float)i + 1})),
+                                    Vector2{ 30, 75 }, BLUE, spawnRot);
+                                std::cout << "FLAG CREATED AT LOCATION: " << nf->coll->m_origin.x << " | " << nf->coll->m_origin.y << '\n';
+
+                                flags.push_back(nf);
+                                nf->flagIndex = flags.size() - 1;
+                                nf->m_previousFlag = flags[nf->flagIndex - 1];
+                                flags[nf->flagIndex - 1]->m_nextFlag = nf;
+
+                            }
+                        }
+                    
+                    
+                    }
+                }
+                flags.push_back(f);
+                f->flagIndex = flags.size() - 1;
+                if (flags.size() != 1) {
+                    flags[f->flagIndex - 1]->m_nextFlag = f;
+                    f->m_previousFlag = flags[f->flagIndex - 1];
+                }
+                f->m_nextFlag = flags[0];
+                flags[0]->m_previousFlag = f;
+
+                //for (int i = 0; i < flags.size(); i++) {
+                //    std::cout << "Flag Data for: " << flags[i]->flagIndex << '\n';
+                //    std::cout << "Previous Flag: " << flags[i]->m_previousFlag->flagIndex << '\n';
+                //    std::cout << "Next Flag: " << flags[i]->m_nextFlag->flagIndex << '\n';
+                //    std::cout << "================================" << '\n';
+                //}
+
+            }
+        }
+        else {
+            if (justSwapped) {
+                std::cout << "Lap start!" << '\n';
+                for (int i = 0; i < flags.size(); i++) {
+                    std::cout << "Flag: " << i << " - Status: " << std::boolalpha << flags[i]->m_hasPassed << '\n';
+                }
+                std::cout << "Lap count: " << lapCounter << '\n';
+                justSwapped = false;
+            
+            }
+
+
+            for (Flag* f : flags) {
+                if (f->coll->CheckForEntry(v)) {
+
+                    system("CLS");
+                    if (f->flagIndex == 0) {
+                        std::cout << "Lap start!" << '\n';
+                        f->m_hasPassed = true;
+
+                        if (f->m_previousFlag->m_hasPassed) {
+                            lapCounter++;
+                            for (int i = 1; i < flags.size(); i++)
+                            {
+                                flags[i]->m_hasPassed = false;
+                            }
+                        }
+
+                    }
+                    else {
+                        if (f->m_previousFlag->m_hasPassed) {
+                            std::cout << "Passed next flag at " << f->flagIndex << '\n';
+                            f->m_hasPassed = true;
+                        }
+                        else {
+                            std::cout << "Flag skipped!!!" << '\n';
+                        }
+                    }
+                    for (int i = 0; i < flags.size(); i++) {
+                        std::cout << "Flag: " << i << " - Status: " << std::boolalpha << flags[i]->m_hasPassed << '\n';
+                    }
+                    std::cout << "Lap count: " << lapCounter << '\n';
+                }
+            }
+        }
+
+        for (Flag* f : flags) {
+            if (f->flagIndex == 0 && !drawingModeEnabled) {
+                f->coll->DrawStart();
+            }
+            f->coll->Draw();
+            if (f->m_nextFlag->m_hasPassed && f->m_nextFlag->flagIndex != 0) {
+                DrawLine(f->coll->m_midPoint.x, f->coll->m_midPoint.y, f->m_nextFlag->coll->m_midPoint.x, f->m_nextFlag->coll->m_midPoint.y, BLUE);
+            }
+            else {
+                DrawLine(f->coll->m_midPoint.x, f->coll->m_midPoint.y, f->m_nextFlag->coll->m_midPoint.x, f->m_nextFlag->coll->m_midPoint.y, RED);
+            }
+        }
+
+
+
+
+
+        
+
+
+
+        
+        lastFrameMPose = v;
+        EndDrawing();
+        //----------------------------------------------------------------------------------
+    }
+
+    // De-Initialization
+    //--------------------------------------------------------------------------------------   
+    CloseWindow();        // Close window and OpenGL context
+    //--------------------------------------------------------------------------------------
+
+    return 0;
+}
